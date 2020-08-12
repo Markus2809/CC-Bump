5c684dbeff398ac436f87ee2f149e0cc41026b59
Markus2809 <60295726+Markus2809@users.noreply.github.com>
2020-08-12 11:05:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   mongodb_connection_uri: process.env.URI, // The URL to connect to the MongoDB database
-  prefix: "a-", // The bots prefix
+  prefix: "your prefix here", // The bots prefix
   token: process.env.TOKEN, // The bots token
-  users_that_are_able_to_add_premium: ["426015209497296908", "492708936290402305"],
+  users_that_are_able_to_add_premium: ["your id here"],
   bump_cooldown: "30 minutes", // The cooldown between bumping
-  support_invite: "https://discord.gg/6957MRs", // The invite for the support server
+  support_invite: "The invite for the support server here", // The invite for the support server
 };
